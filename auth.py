4b9a92a11ef528929a27f4680c25d6d166214b0c
Duke-Stark <chaharchandresh@gmail.com>
2020-07-17 09:20:33 +0530

    added environment variables

--- auth.py
+++ auth.py
@@ -1,13 +1,14 @@
 import json
+import os
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'dev-q8b9smth.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'myapp'
+AUTH0_DOMAIN = os.environ['AUTH0_D']
+ALGORITHMS = [os.environ['AUTH0_ALGO']]
+API_AUDIENCE = os.environ['AUTH0_API']
 
 ## AuthError Exception
 '''
